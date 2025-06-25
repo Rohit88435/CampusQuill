b7120d75d33c25db974ec4bc52409f2ca4e652d2
Rohit88435 <kuprajapatir@gmail.com>
2025-06-25 08:26:32 +0530

    final commit

--- Backend/Routes/auth.routes.js
+++ Backend/Routes/auth.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { login, logout, signup } from "../Controller/auth.controllers.js";
+import isAuth from "../MiddleWares/isAuth.js";
 
 // instance of router form express
 export let authRouter = express.Router();
 
 // signup post
-authRouter.post("/signup", signup);
+authRouter.post("/signup", isAuth, signup);
 
-authRouter.post("/login", login);
+authRouter.post("/login", isAuth, login);
 
-authRouter.get("/logout", logout);
+authRouter.get("/logout", isAuth, logout);
